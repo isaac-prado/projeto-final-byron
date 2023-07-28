9226ce27d34cdaa32b96673ac61ecd21be20ceb9
Isaac <isaacpradoalmeida@gmail.com>
2023-07-28 14:41:23 -0300

    modal finalizado e pets responsiva

--- src/app/components/MainPage.tsx
+++ src/app/components/MainPage.tsx
@@ -1,79 +1,39 @@
-"use client";
-import Link from "next/link";
-import petsData from "./data";
+"use client"
+import Link from "next/link"
+import cardList from "./data"
 
 const MainPage = () => {
-    return (
-        <div className="mt-20 md:p-10 p-6">
-            <div className="text-center">
-                <h1 className="text-2xl font-semibold">
-                    Conheça o Nosso Projeto
-                </h1>
-                <p className="pt-4 md:mx-20">
-                    Bem-vindo ao Segunda Chance! Nosso objetivo é promover a
-                    adoção responsável e encontrar lares amorosos para animais
-                    de estimação que estão ansiosos para encontrar uma família
-                    para chamarem de sua. Aqui, acreditamos que todos os animais
-                    merecem um lar seguro, cheio de amor e cuidado. Nosso site
-                    serve como um elo entre esses animais adoráveis ​​e pessoas
-                    maravilhosas, como você, que estão prontas para abrir seus
-                    corações e lares para um novo membro da família peludo.
-                </p>
-            </div>
-            <div className="text-center">
-                <h1 className="text-2xl font-semibold p-6">
-                    Encontre seu novo Amigo
-                </h1>
-            </div>
+  return (
+    <div className="mt-20 md:p-10 p-6">
+        <div className="text-center">
+            <h1 className="text-2xl font-semibold">Conheça o Nosso Projeto</h1>
+            <p className="pt-4 md:mx-20">Bem-vindo ao Segunda Chance! Nosso objetivo é promover a adoção responsável e encontrar lares amorosos para animais de estimação que estão ansiosos para encontrar uma família para chamarem de sua. Aqui, acreditamos que todos os animais merecem um lar seguro, cheio de amor e cuidado. Nosso site serve como um elo entre esses animais adoráveis ​​e pessoas maravilhosas, como você, que estão prontas para abrir seus corações e lares para um novo membro da família peludo.</p>
+        </div>
+        <div className="text-center">
+            <h1 className="text-2xl font-semibold p-6">Encontre seu novo Amigo</h1>
+        </div>
 
-            <main className="container mx-auto">
+        <main className="container mx-auto">
                 <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-2">
-                    {petsData.map((card) => (
+                    {cardList.map(card => (
                         <Link href={`/pages/pets/`}>
                             <div className="border-2 border-gray-300 shadow-lg rounded-lg cursor-pointer bg-slate-100-100 hover:bg-slate-300 duration-300">
-                                <img
-                                    className="md:h-48 h-32 w-full  rounded-t-lg"
-                                    src={card.img}
-                                    alt=""
-                                />
+                                <img className="md:h-48 h-32 w-full  rounded-t-lg" src={card.img} alt=""/>
                                 <div className="p-4">
-                                    <h3 className="text-xl font-semibold text-marrom_escuro mb-2">
-                                        {card.name}
-                                    </h3>
+                                    <h3 className="text-xl font-semibold text-marrom_escuro mb-2">{card.name}</h3>
                                     <div className="tex-lg font-medium text-gray-800 justify-center items-start">
-                                        <p className="flex gap-2 mb-3">
-                                            <img
-                                                className="h-6"
-                                                src="./footer/pin-de-localizacao.png"
-                                                alt=""
-                                            />
-                                            {card.city}
-                                        </p>
-                                        <p className="flex gap-2 mb-3">
-                                            <img
-                                                className="h-6"
-                                                src="./footer/pata-de-cachorro.png"
-                                                alt=""
-                                            />{" "}
-                                            {card.animal}
-                                        </p>
-                                        <p className="flex gap-2">
-                                            <img
-                                                className="h-6"
-                                                src="./footer/tamanho-cachorro.png"
-                                                alt=""
-                                            />
-                                            {card.size}
-                                        </p>
+                                        <p className="flex gap-2 mb-3"><img className="h-6" src="./footer/pin-de-localizacao.png" alt="" />{card.city}</p> 
+                                        <p className="flex gap-2 mb-3"><img className="h-6" src="./footer/pata-de-cachorro.png" alt="" />  {card.animal}</p> 
+                                        <p className="flex gap-2"><img className="h-6" src="./footer/tamanho-cachorro.png" alt="" />{card.size}</p> 
                                     </div>
                                 </div>
                             </div>
                         </Link>
                     ))}
                 </div>
-            </main>
-        </div>
-    );
-};
+        </main>
+    </div>
+  )
+}
 
-export default MainPage;
+export default MainPage
--- src/app/pages/pets/page.tsx
+++ src/app/pages/pets/page.tsx
@@ -8,25 +8,27 @@ const Pets = () => {
     return (
         <div className="mt-28 md:mx-20 m-6">
             <div className="flex-col w-full">
-                <div>
-                    <div className="text-center">
-                        <div className="text-3xl font-bold text-marrom_escuro">
-                            AURÉLIO
-                        </div>
-                        <div className="text-lg text-marrom_claro">
-                            Cachorro | Macho | Filhote | Porte Pequeno
-                        </div>
-                    </div>
-                    <div className="font-bold pt-5">
+                <div className="md:grid md:grid-cols-3">
+                    <div className="text-center md:text-left md:col-start-2 md:col-span-2">
                         <div>
-                            <p className="flex gap-2"><img src="./footer/pin-de-localizacao.png" alt="Pin de localização" />Itajubá, MG</p>
+                            <div className="text-3xl font-bold text-marrom_escuro md:col-start-2">
+                                AURÉLIO
+                            </div>
+                            <div className="text-lg text-marrom_claro">
+                                Cachorro | Macho | Filhote | Porte Pequeno
+                            </div>
                         </div>
-                        <div className="mt-2 ml-1">
-                            <p className="flex gap-2"><img src="./footer/homem-usuario.png" alt="Usuário que publicou" />Publicado por Solidaripatas</p>
+                        <div className="font-bold pt-5">
+                            <div>
+                                <p className="flex gap-2"><img src="./footer/pin-de-localizacao.png" alt="Pin de localização" />Itajubá, MG</p>
+                            </div>
+                            <div className="mt-2 ml-1">
+                                <p className="flex gap-2"><img src="./footer/homem-usuario.png" alt="Usuário que publicou" />Publicado por Solidaripatas</p>
+                            </div>
                         </div>
                     </div>
 
-                    <div>
+                    <div className="md:col-start-1 md:mr-8">
                         <div className="items-center justify-center mt-4">
                             <img className="w-full rounded-md" src="./images/dog3.jpeg" alt="" />
                         </div>
@@ -38,24 +40,26 @@ const Pets = () => {
                         </div>
                     </div>
 
-                    <div className="text-center mt-4">
-                        <p className="text-2xl font-semibold text-marrom_escuro">Bio</p>
+                    <div className="text-center md:text-left md:mt-12 mt-4 md:col-span-2">
+                        <p className="text-2xl font-semibold text-marrom_escuro md:mb-4">A história do Aurélio</p>
                         <p>Aurelio é um cão de porte médio com uma pelagem caramelo brilhante e olhos expressivos que irão derreter seu coração. Ele foi resgatado das ruas e acolhido em um abrigo, onde mostrou uma personalidade brincalhona e afetuosa desde o início. Com aproximadamente dois anos de idade, Aurelio é um companheiro animado e energético. Ele adora correr, brincar com uma bola e explorar o mundo ao seu redor. Seu espírito aventureiro vai te inspirar a aproveitar a vida ao máximo. Aurelio também é um cachorro extremamente inteligente e aprende com rapidez. Ele está ansioso para encontrar um dono dedicado que possa continuar a estimular sua mente ágil e ensinar truques divertidos. Com amor e paciência, Aurelio tem todo o potencial para se tornar um companheiro leal e obediente.</p>
                     </div>
 
-                    <div className="text-center font-semibold mt-4">
-                        <button onClick={() => setOpenModal(true)} className="text-2xl bg-red-300 py-4 px-7 mt-6 rounded-lg shadow-lg">
-                            QUERO ADOTAR
-                        </button>
-                    </div>
-                    <Modal isOpen={openModal} setOpenModal={() => setOpenModal(!openModal)}>
-                        <div className="flex-col items-center text-center gap-4 font-bold">
-                            <h2 className="text-3xl mb-4">Quer Adotar?</h2>
-                            <p>Para adotar esse pet ou saber mais sobre ele, entre em contato com o dono:</p>
-                            <p className="mt-4 gap-2"><img src="./footer/whatsapp.png" alt="" />(xx)xxxx-xxxx</p>
-                            <p className="gap-2"><img src="./footer/email.png" alt="" />email@email.com</p>
+                    <div className="md:col-start-2">
+                        <div className="text-center font-semibold mt-4">
+                            <button onClick={() => setOpenModal(true)} className="text-2xl bg-red-300 py-4 px-7 mt-6 rounded-lg shadow-lg">
+                                QUERO ADOTAR
+                            </button>
                         </div>
-                    </Modal>
+                        <Modal isOpen={openModal} setOpenModal={() => setOpenModal(!openModal)}>
+                            <div className="flex-col items-center text-center gap-4 font-bold">
+                                <h2 className="text-3xl mb-4">Quer Adotar?</h2>
+                                <p>Para adotar esse pet ou saber mais sobre ele, entre em contato com o dono:</p>
+                                <p className="mt-4"><img src="./footer/whatsapp.png" alt="" />(xx)xxxx-xxxx</p>
+                                <p><img src="./footer/email.png" alt="" />email@email.com</p>
+                            </div>
+                        </Modal>
+                    </div>
 
                 </div>
             </div>
